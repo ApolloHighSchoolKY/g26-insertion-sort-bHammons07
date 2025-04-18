f57a10532fdd5dc4ca0767aca12186d42df21b4c
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-04-18 14:10:26 +0000

    gog

--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -10,19 +10,33 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int pos = 1; pos < myNumbers.length; pos++)
+		{
+			inserted = false;
 			//Check with each of the numbers in the sorted list
-
+			for(int i = 0; i < sorted.size(); i++)
+			{
 				//If this number is less than one in the sorted list,
-				//insert it there
-
+				if(myNumbers[pos] < sorted.get(i))
+				{
+					//insert it there
+					sorted.add(i, myNumbers[pos]);
+					inserted = true;
+				}
+				
+			}
+				
 			//If it was not inserted, stick it on the end.
-
+			if(inserted==false)
+			{
+				sorted.add(myNumbers[pos]);
+			}
 
 		//End Loop for unsorted list
-
+		}
+			
 		System.out.println(sorted);
 
 		//Move the data back to the array
